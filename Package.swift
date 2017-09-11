449c286dda9c96fc2946fd1ffbd18365eacde76d
Valtteri Koskivuori <vkoskiv@gmail.com>
2017-09-11 06:19:29 +0300

    "created VaporCoin from template https://github.com/vapor/api-template"

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "VaporCoin",
+    targets: [
+        Target(name: "App"),
+        Target(name: "Run", dependencies: ["App"]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+    ]
+)
+
