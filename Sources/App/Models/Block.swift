006d8ab39ab15489aaa41b07b980be13ab8461c3
Valtteri Koskivuori <vkoskiv@gmail.com>
2017-09-13 12:32:17 +0300

    Refactor Block

--- Sources/App/Models/Block.swift
+++ Sources/App/Models/Block.swift
@@ -10,74 +10,90 @@ import Vapor
 import Foundation
 
 final class Block: NSObject, NSCoding {
+	//Block header
+	var prevHash: Data
+	var merkleRoot: MerkleRoot
+	var timestamp: Double //Unix Tstamp
+	var target: Float
+	var nonce: Int64 //256 bit hash
+	
+	var depth: Int
+	var txns: [Transaction]
+	
+	var blockHash: Data {
+		return self.encoded().sha256
+	}
+	
+	override init() {
+		self.prevHash = Data()
+		self.merkleRoot = MerkleRoot()
+		self.timestamp = Date().timeIntervalSince1970
+		self.target = 1
+		self.nonce = 0
+		
+		self.depth = 0
+		self.txns = []
+		
+	}
+	
+	init(prevHash: Data, merkleRoot: MerkleRoot, depth: Int, txns: [Transaction], timestamp: Double, difficulty: Float, nonce: Int64) {
+		self.prevHash = prevHash
+		self.merkleRoot = merkleRoot
+		self.depth = depth
+		self.txns = txns
+		self.timestamp = timestamp
+		self.target = difficulty
+		self.nonce = nonce
+	}
+	
+	func encoded() -> Data {
+		return NSKeyedArchiver.archivedData(withRootObject: self)
+	}
+	
+	func verify() -> Bool {
+		//Verify the validity of a block
+		
+		//Verify the hash
+		//Check that the reported hash matches
+		let testHash = self.encoded().sha256
+		if self.blockHash != testHash {
+			print("Block hash doesn't match")
+			return false
+		}
+		//Check that hash is valid
+		
+		
+		//Check timestamp
+		
+		return true
+	}
 	
 	//MARK: Swift encoding logic
-	
 	public convenience required init?(coder aDecoder: NSCoder) {
 		let prevHash = aDecoder.decodeObject(forKey: "prevHash") as! Data
+		let merkleRoot = aDecoder.decodeObject(forKey: "merkleTree") as! MerkleRoot
 		let depth = aDecoder.decodeInteger(forKey: "depth")
 		let txns = aDecoder.decodeObject(forKey: "txns") as! [Transaction]
 		let timestamp = aDecoder.decodeDouble(forKey: "timestamp")
 		let difficulty = aDecoder.decodeFloat(forKey: "difficulty")
 		let nonce = aDecoder.decodeInt64(forKey: "nonce")
-		let blockHash = aDecoder.decodeObject(forKey: "hash") as! Data
 		
-		self.init(prevHash: prevHash, depth: depth, txns: txns, timestamp: timestamp, difficulty: difficulty, nonce: nonce, hash: blockHash)
+		self.init(prevHash: prevHash, merkleRoot: merkleRoot, depth: depth, txns: txns, timestamp: timestamp, difficulty: difficulty, nonce: nonce)
 	}
 	
 	func encode(with aCoder: NSCoder) {
 		aCoder.encode(prevHash, forKey: "prevHash")
-		aCoder.encode(depth, forKey: "depth")
-		aCoder.encode(txns, forKey: "txns")
+		aCoder.encode(merkleRoot, forKey: "merkleRoot")
 		aCoder.encode(timestamp, forKey: "timestamp")
-		aCoder.encode(difficulty, forKey: "difficulty")
+		aCoder.encode(target, forKey: "target")
 		aCoder.encode(nonce, forKey: "nonce")
-		aCoder.encode(blockHash, forKey: "blockHash")
-	}
-	
-	//Class
-	var prevHash: Data
-	var depth: Int
-	var txns: [Transaction]
-	var timestamp: Double //Unix Tstamp
-	
-	var difficulty: Float
-	
-	var nonce: Int64 //256 bit hash
-	var blockHash: Data
-	
-	override init() {
-		self.prevHash = Data()
-		self.depth = 0
-		self.txns = []
-		self.timestamp = Date().timeIntervalSince1970
-		self.difficulty = 1
-		self.nonce = 0
-		self.blockHash = Data()
-	}
-	
-	init(prevHash: Data, depth: Int, txns: [Transaction], timestamp: Double, difficulty: Float, nonce: Int64, hash: Data) {
-		self.prevHash = prevHash
-		self.depth = depth
-		self.txns = txns
-		self.timestamp = timestamp
-		self.difficulty = difficulty
-		self.nonce = nonce
-		self.blockHash = hash
-	}
-	
-	func encoded() -> Data {
-		//Encode block into a data stream, for hashes
-		//TODO
-		return Data()
-	}
-	
-	func verify() -> Bool {
-		return true
+		
+		/*aCoder.encode(depth, forKey: "depth")
+		aCoder.encode(txns, forKey: "txns")*/
 	}
 }
 
 func genesisBlock() -> Block {
-	let genesis = Block(prevHash: Data(), depth: 0, txns: [], timestamp: 1505278315, difficulty: 1.0, nonce: 0, hash: Data())
+	let genesis = Block(prevHash: Data(), merkleRoot: MerkleRoot(), depth: 0, txns: [], timestamp: 1505278315, difficulty: 1.0, nonce: 0)
 	return genesis
 }
