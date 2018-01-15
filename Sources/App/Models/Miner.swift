151b62915b3b546d08512f1b79ecb1242c236d3e
John Pope <jp@fieldstormapp.com>
2018-01-15 17:32:09 -0500

    reduce thread count back to 4

--- Sources/App/Models/Miner.swift
+++ Sources/App/Models/Miner.swift
@@ -65,7 +65,7 @@ class Miner {
         state.blockFound = false
         state.blockFoundQueue.cancelAllOperations()
         
-        let miner = Miner(coinbase: "coinbaseAddressNotImplementedYet", diff: 20, threadCount: 400)
+        let miner = Miner(coinbase: "coinbaseAddressNotImplementedYet", diff: 20, threadCount: 4)
         let block = Block(prevHash: state.getPreviousBlock().blockHash, depth: state.blockChain.count, txns: [Transaction()], timestamp: Date().timeIntervalSince1970, difficulty: 5000, nonce: 0, hash: Data())
         
         block.nonce = 0
