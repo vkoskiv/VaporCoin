78db09ec1fba4e5504237d42e74e973422560dfd
Valtteri Koskivuori <vkoskiv@gmail.com>
2018-04-07 22:25:14 +0300

    Compute merkleRoot only when modifying `txns` array, and store it.

--- Sources/App/Models/Block.swift
+++ Sources/App/Models/Block.swift
@@ -12,19 +12,25 @@ import Foundation
 class Block {
 	//Block header
 	var prevHash: Data
-	//FIXME: merkleRoot is computed every single time. Find a way to set+store it whenever transactions are altered.
-	var merkleRoot: Data {
-		return MerkleRoot.getRootHash(fromTransactions: self.txns)
-	}
+	var merkleRoot: Data
 	var timestamp: Double //Unix Tstamp
 	var target: Float
 	var nonce: UInt32 //32 bit nonce
-	
 	var depth: Int
-	var txns: [Transaction]
-
+	
+	//Block contents
+	var txns: [Transaction] {
+		didSet {
+			self.merkleRoot = MerkleRoot.getRootHash(fromTransactions: self.txns)
+		}
+	}
+	
 	var blockHash: Data {
 		return self.encoded.sha256
+	}
+	
+	var encoded: Data {
+		return prevHash + merkleRoot + Data(from: depth) + Data(from: timestamp) + Data(from: target) + Data(from: nonce)
 	}
 	
 	func newCopy() -> Block {
@@ -44,10 +50,7 @@ class Block {
 		self.timestamp = timestamp
 		self.target = difficulty
 		self.nonce = nonce
-	}
-	
-	var encoded: Data {
-		return prevHash + merkleRoot + Data(from: depth) + Data(from: timestamp) + Data(from: target) + Data(from: nonce)
+		self.merkleRoot = MerkleRoot.getRootHash(fromTransactions: self.txns)
 	}
 }
 
@@ -55,3 +58,4 @@ func genesisBlock() -> Block {
 	let genesis = Block(prevHash: Data(Bytes(repeatElement(0, count: 32))), depth: 0, txns: [], timestamp: 1505278315, difficulty: 1.0, nonce: 0)
 	return genesis
 }
+
